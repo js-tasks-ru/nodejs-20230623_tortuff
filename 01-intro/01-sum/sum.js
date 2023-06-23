8a97fc88a1498e8a1f481b52f4b27bbbd29ea9c8
Tortuff <zhekabely12@gmail.com>
2023-06-23 22:23:26 +0200

    Initial task. Sum function has been implemented.

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,13 @@
 function sum(a, b) {
-  /* ваш код */
+  if (isNumber(a) && isNumber(b)) {
+    return a + b;
+  } else {
+    throw new TypeError('Some argument is incorrect.');
+  }
+}
+
+function isNumber(num) {
+  return typeof num === 'number' && !isNaN(num);
 }
 
 module.exports = sum;
