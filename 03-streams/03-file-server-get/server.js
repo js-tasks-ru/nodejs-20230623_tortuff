e27299480ae15ba59040f2f214b1f0b2838558f7
Tortuff <zhekabely12@gmail.com>
2023-07-07 00:05:30 +0200

    The File server has been implemented.

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,6 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('node:fs');
 
 const server = new http.Server();
 
@@ -8,17 +8,40 @@ server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
+  if (pathname.includes('/')) {
+    res.statusCode = 400;
+    res.end('Incorrect path');
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
-
+      readFileFrom(res, filepath);
       break;
 
     default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+      res.statusCode = 500;
+      res.end('Internal server error');
   }
 });
+
+function readFileFrom(res, filepath) {
+  const readStream = fs.createReadStream(filepath);
+
+  readStream.on('error', err => {
+    if (err.code === 'ENOENT') {
+      res.statusCode = 404;
+      res.end(err.message);
+    } else {
+      res.statusCode = 500;
+      res.end('Internal server error');
+    }
+  });
+
+  server.on('close', () => readStream.close());
+
+  readStream.pipe(res);
+}
 
 module.exports = server;
