9bb6c7174ef29755ba89834bc88bab300b8993c6
Yevhenii Bielykh <evgeniy98belykh@gmail.com>
2024-01-28 16:58:56 +0100

    Emails. Part 1. The registration flow by email has been implemented.

--- 08-email/01-registration/app.js
+++ 08-email/01-registration/app.js
@@ -1,15 +1,15 @@
 const path = require('path');
 const Koa = require('koa');
-const {v4: uuid} = require('uuid');
+const { v4: uuid } = require('uuid');
 const Router = require('koa-router');
 const handleMongooseValidationError = require('./libs/validationErrors');
 const mustBeAuthenticated = require('./libs/mustBeAuthenticated');
-const {productsBySubcategory, productList, productById} = require('./controllers/products');
-const {categoryList} = require('./controllers/categories');
-const {login} = require('./controllers/login');
-const {oauth, oauthCallback} = require('./controllers/oauth');
-const {me} = require('./controllers/me');
-const {register, confirm} = require('./controllers/registration');
+const { productsBySubcategory, productList, productById } = require('./controllers/products');
+const { categoryList } = require('./controllers/categories');
+const { login } = require('./controllers/login');
+const { oauth, oauthCallback } = require('./controllers/oauth');
+const { me } = require('./controllers/me');
+const { register, confirm } = require('./controllers/registration');
 const Session = require('./models/Session');
 
 const app = new Koa();
@@ -23,11 +23,11 @@ app.use(async (ctx, next) => {
   } catch (err) {
     if (err.status) {
       ctx.status = err.status;
-      ctx.body = {error: err.message};
+      ctx.body = { error: err.message };
     } else {
       console.error(err);
       ctx.status = 500;
-      ctx.body = {error: 'Internal server error'};
+      ctx.body = { error: 'Internal server error' };
     }
   }
 });
@@ -35,7 +35,7 @@ app.use(async (ctx, next) => {
 app.use((ctx, next) => {
   ctx.login = async function(user) {
     const token = uuid();
-    await Session.create({token, user, lastVisit: new Date()});
+    await Session.create({ token, user, lastVisit: new Date() });
 
     return token;
   };
@@ -43,7 +43,7 @@ app.use((ctx, next) => {
   return next();
 });
 
-const router = new Router({prefix: '/api'});
+const router = new Router({ prefix: '/api' });
 
 router.use(async (ctx, next) => {
   const header = ctx.request.get('Authorization');
@@ -52,7 +52,7 @@ router.use(async (ctx, next) => {
   const token = header.split(' ')[1];
   if (!token) return next();
 
-  const session = await Session.findOne({token}).populate('user');
+  const session = await Session.findOne({ token }).populate('user');
   if (!session) {
     ctx.throw(401, 'Неверный аутентификационный токен');
   }
--- 08-email/01-registration/controllers/registration.js
+++ 08-email/01-registration/controllers/registration.js
@@ -1,11 +1,53 @@
-const { v4: uuid } = require('uuid');
+const {v4: uuid} = require('uuid');
 const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+  const {email, password, displayName} = ctx.request.body;
 
+  if (!(email?.trim() && password?.trim() && displayName?.trim())) {
+    return ctx.throw(400, 'All fields are required');
+  }
+
+  const token = uuid();
+
+  try {
+    const user = new User({
+      verificationToken: token,
+      email,
+      password,
+      displayName,
+    });
+
+    await user.setPassword(password);
+    await user.save();
+
+    await sendMail({
+      template: 'confirmation',
+      locals: {token},
+      to: email,
+      subject: 'Подтвердите почту',
+    });
+
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+
+  ctx.body = {status: 'ok'};
 };
 
 module.exports.confirm = async (ctx, next) => {
+  const {verificationToken} = ctx.request.body;
+  if (!verificationToken) return ctx.throw(400, 'Invalid token provided');
 
+  const user = await User.findOneAndUpdate(
+    {verificationToken},
+    {$unset: {verificationToken: 1}},
+  );
+
+  if (!user) {
+    return ctx.throw(400, 'Ссылка подтверждения недействительна или устарела');
+  }
+
+  ctx.body = {token: verificationToken};
 };
